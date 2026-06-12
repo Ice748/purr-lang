b21b2efab8c132f318855bdcb76d70a80f1862ba
Ice748 <dvinogradov174@gmail.com>
2026-06-12 12:14:58 +0300

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,7 +1,15 @@
 @echo off
 echo Installing meow-lang...
-mkdir C:\meow
-copy meow.exe C:\meow\meow.exe
-setx PATH "%PATH%;C:\meow"
+
+mkdir C:\meow 2>nul
+copy meow.exe C:\meow\meow.exe >nul
+
+for /f "tokens=2*" %%a in ('reg query "HKCU\Environment" /v Path 2^>nul') do set "OLD_PATH=%%b"
+if defined OLD_PATH (
+    reg add "HKCU\Environment" /v Path /t REG_EXPAND_SZ /d "%OLD_PATH%;C:\meow" /f >nul
+) else (
+    reg add "HKCU\Environment" /v Path /t REG_EXPAND_SZ /d "C:\meow" /f >nul
+)
+
 echo Done! Please restart your terminal.
 pause
